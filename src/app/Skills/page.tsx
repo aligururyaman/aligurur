c78728c039e1068eb229ab93bfcbef0420455148
Ali Gurur Yaman <aligururyaman@gmail.com>
2024-08-03 12:20:24 +0300

    first

--- src/app/Skills/page.tsx
+++ src/app/Skills/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 import React from "react";
-
 import { AnimatePresence, motion } from "framer-motion";
 import { CanvasRevealEffect } from "@/components/ui/canvas-reveal-effect";
 import { FaReact, FaDatabase } from "react-icons/fa";
 import { TbBrandReactNative } from "react-icons/tb";
 import { RiNextjsFill } from "react-icons/ri";
-
 import {
   SiAdobe,
   SiAdobeaftereffects,
@@ -14,7 +12,7 @@ import {
   SiAdobephotoshop,
 } from "react-icons/si";
 
-function page() {
+function Page() {
   return (
     <>
       <div className="py-20 flex flex-wrap lg:flex-row items-center justify-center bg-white dark:bg-black w-full gap-4 mx-auto px-8 text-4xl">
@@ -71,7 +69,7 @@ function page() {
   );
 }
 
-export default page;
+export default Page;
 
 const Card = ({
   title,
@@ -87,7 +85,7 @@ const Card = ({
     <div
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
-      className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative h-[30rem] "
+      className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative h-[30rem]"
     >
       <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
       <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
@@ -110,7 +108,7 @@ const Card = ({
         <div className="text-center group-hover/canvas-card:-translate-y-4 group-hover/canvas-card:opacity-0 transition duration-200 w-full  mx-auto flex items-center justify-center">
           {icon}
         </div>
-        <h2 className="dark:text-white text-xl opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200">
+        <h2 className="dark:text-white text-xl opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4 font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200">
           {title}
         </h2>
       </div>
